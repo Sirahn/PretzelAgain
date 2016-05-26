7e6cc9bdc3f2b512c9987e1b2f66c3e485b32976
Your Name <you@example.com>
2016-05-26 10:28:00 -0400

    Revisiting Javascript Pretzel app

---
+++ PretzelAgain/JavaScript.js
@@ -0,0 +1,18 @@
+﻿// JavaScript source code
+function calculateTotal() {
+    var pretzel = 3.99;
+    var userInput = document.getElementById("quantity");
+    var userSelection = userInput.elements["pretzelNum"].value; //get value of object
+    var totalSale = pretzel * userSelection;
+    totalSale = roundFloat(totalSale, 2);
+    document.getElementById("receipt").innerHTML = totalSale;
+
+
+}
+function roundFloat(num, dec) {
+    var d = 1;
+    for (var i = 0; i < dec; i++) {
+        d += "0";
+    }
+    return Math.round(num * d) / d;
+}
